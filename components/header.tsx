c8af6e01134ecbaba3836e7e94f4477dd7a181cb
Anthony Schneider <tonyschneider3@gmail.com>
2024-07-15 13:08:35 -0500

    header: changed page title

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "@/components/theme-toggle";
 export function Header() {
   return (
     <header className="flex items-center justify-between p-4">
-      <h1 className="text-2xl font-bold">Nextjs Shadcn Template</h1>
+      <h1 className="text-2xl font-bold">yt-dlp online</h1>
       <ThemeToggle />
     </header>
   );
